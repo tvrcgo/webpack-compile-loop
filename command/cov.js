a56ce1c592872dcddbd378e1ab357a948cfd7155
tvrcgo <tvrcgo@gmail.com>
2017-07-02 10:37:15 +0800

    feat: better cov

--- command/cov.js
+++ command/cov.js
@@ -2,13 +2,17 @@ const { resolve } = require('path')
 
 module.exports = function* (argv, cmd) {
   const nyc = require.resolve('nyc/bin/nyc.js')
+  const loop = resolve(__dirname, '../bin/loop.js')
   const args = [
-    'loop',
+    '--temp-directory', './node_modules/.nyc_output',
+    '--report-dir', 'cov',
+    loop,
     'test'
   ]
   const opt = {
     env: {
-      NODE_ENV: 'test'
+      NODE_ENV: 'test',
+      FORCE_COLOR: true
     }
   }
   yield cmd.fork(nyc, args, opt)
